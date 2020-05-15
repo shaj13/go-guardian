874af628c5ea87c40298c09e2279cc1268bec95a
Sanad <s.hajyahya@f5.com>
2020-05-15 15:01:44 -0700

    fix: add  2fa key and digits

--- TFA/otp.go
+++ TFA/otp.go
@@ -1,64 +1,43 @@
 package TFA
 
 import (
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
-	"hash"
 	"time"
 )
 
 type OTP interface {
 	Interval() uint64
-	SetInterval(i uint64) 
+	SetInterval(i uint64)
 	Secret() string
-	Hasher() func() hash.Hash
+	Algorithm() HashAlgorithm
 	Digits() int
 	Verify(otp string) (bool, error)
 }
 
 type baseOTP struct {
-	interval uint64
-	secret   string
-	digits   int
+	interval  uint64
+	secret    string
+	digits    int
+	algorithm HashAlgorithm
 }
-
-func (b *baseOTP) Interval() uint64 {
-	return b.interval
-}
-
-func (b *baseOTP) Secret() string {
-	return b.secret
-}
-
-func (b *baseOTP) Digits() int {
-	return b.digits
-}
+func (b *baseOTP) Interval() uint64         { return b.interval }
+func (b *baseOTP) Secret() string           { return b.secret }
+func (b *baseOTP) Digits() int              { return b.digits }
+func (b *baseOTP) Algorithm() HashAlgorithm { return b.algorithm }
 
 type totp struct {
 	*baseOTP
-	algorithm HashAlgorithm
+	period uint64
 }
 
 func (t *totp) Verify(otp string) (bool, error) {
-	code, err := GeneratCode(t)
+	t.SetInterval(t.period)
+	code, err := GeneratOTP(t)
 	return code == otp, err
 }
 
 func (t *totp) SetInterval(i uint64) {
-	t.interval = uint64(time.Now().UTC().Unix())
-}
-
-func (t *totp) Hasher() func() hash.Hash {
-	switch t.algorithm {
-	case SHA1:
-		return sha1.New
-	case SHA256:
-		return sha256.New
-	case SHA512:
-		return sha512.New
-	}
-	return nil
+	t.period = i
+	t.interval = uint64(time.Now().UTC().Unix()) / t.period
 }
 
 type hotp struct {
@@ -66,30 +45,26 @@ type hotp struct {
 }
 
 func (h *hotp) Verify(otp string) (bool, error) {
-	h.SetInterval(h.interval+1)
-	code, err := GeneratCode(h)
+	h.SetInterval(h.interval + 1)
+	code, err := GeneratOTP(h)
 	return code == otp, err
-}
-
-func (h *hotp) Hasher() func() hash.Hash {
-	return sha1.New
 }
 
 func (h *hotp) SetInterval(i uint64) {
 	h.interval = i
 }
 
-func newBaseOTP( secret string, digits int) *baseOTP {
+func newBaseOTP(secret string, digits int, algo HashAlgorithm) *baseOTP {
 	return &baseOTP{
-		secret: secret,
-		digits: digits,
+		secret:    secret,
+		digits:    digits,
+		algorithm: algo,
 	}
 }
 
-func newTOTP(base *baseOTP, algo HashAlgorithm) *totp { 
+func newTOTP(base *baseOTP) *totp {
 	return &totp{
 		baseOTP: base,
-		algorithm: algo,
 	}
 }
 
--- TFA/tfa.go
+++ TFA/tfa.go
@@ -4,10 +4,16 @@ import (
 	"bytes"
 	"crypto/hmac"
 	"crypto/rand"
+	"crypto/sha1"
+	"crypto/sha256"
+	"crypto/sha512"
 	"encoding/base32"
 	"encoding/binary"
 	"errors"
+	"fmt"
+	"hash"
 	"math"
+	"net/url"
 	"strconv"
 	"strings"
 )
@@ -15,18 +21,124 @@ import (
 // ErrWeakSecretSize is returned by GenerateSecret when input secret size does not meet RFC 4226 requirements.
 var ErrWeakSecretSize = errors.New("Weak secret size, The shared secret MUST be at least 128 bits")
 
-type HashAlgorithm uint8
+// HashAlgorithm represents the hashing function to use in the HMAC
+type HashAlgorithm string
 
 const (
-	SHA1 HashAlgorithm = iota
-	SHA256
-	SHA512
+	SHA1   = HashAlgorithm("SHA1")
+	SHA256 = HashAlgorithm("SHA256")
+	SHA512 = HashAlgorithm("SHA512")
 )
 
-// GenerateCode return one time password or an error if occurs
+func (h HashAlgorithm) Hasher() func() hash.Hash {
+	return map[HashAlgorithm]func() hash.Hash{
+		SHA1:   sha1.New,
+		SHA256: sha256.New,
+		SHA512: sha512.New,
+	}[h]
+}
+
+// Digits represents the length of OTP.
+type Digits int
+
+const (
+	// SixDigits of OTP.
+	SixDigits Digits = 6
+	// EightDigits of OTP
+	EightDigits Digits = 8
+)
+
+//
+func (d Digits) String() string {
+	return fmt.Sprintf("%d", d)
+}
+
+// Key represnt Uri Format for OTP
+// See https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+type Key struct{ *url.URL }
+
+// Type returns the type for the Key (totp, hotp).
+func (k *Key) Type() string {
+	return k.Host
+}
+
+// Label returns the label for the Key.
+func (k *Key) Label() string {
+	return strings.TrimPrefix(k.Path, "/")
+}
+
+// Secret returns the secret for the Key.
+func (k *Key) Secret() string {
+	return k.Query().Get("secret")
+}
+
+// Digits returns the length of pin code.
+func (k *Key) Digits() Digits {
+	str := k.Query().Get("digits")
+	d, err := strconv.Atoi(str)
+	if err != nil {
+		return SixDigits
+	}
+	return Digits(d)
+}
+
+// Issuer returns a string value indicating the provider or service.
+func (k *Key) Issuer() string {
+	return k.Query().Get("issuer")
+}
+
+// IssuerLabelPrefix returns a string value indicating the provider or service extracted from label.
+func (k *Key) IssuerLabelPrefix() string {
+	sub := strings.Split(k.Label(), ":")
+	if len(sub) == 2 {
+		return sub[0]
+	}
+	return ""
+}
+
+// AccountName returns the name of the user's account.
+func (k *Key) AccountName() string {
+	sub := strings.Split(k.Label(), ":")
+	if len(sub) == 2 {
+		return sub[1]
+	}
+	return ""
+}
+
+// Algorithm return the hashing Algorithm name
+func (k *Key) Algorithm() HashAlgorithm {
+	return HashAlgorithm(k.Query().Get("algorithm"))
+}
+
+// Period that a TOTP code will be valid for, in seconds. The default value is 30.
+// if type not a topt the returned value is 0
+func (k *Key) Period() uint64 {
+	if k.Type() == "totp" {
+		if period := k.Query().Get("period"); len(period) > 0 {
+			p, _ := strconv.ParseUint(period, 10, 64)
+			return p
+		}
+		return 30
+	}
+	return 0
+}
+
+// Counter return initial counter value. for provisioning a key for use with HOTP
+// // if type not a hopt the returned value is 0
+func (k *Key) Counter() uint64 {
+	if k.Type() == "hotp" {
+		if counter := k.Query().Get("counter"); len(counter) > 0 {
+			p, _ := strconv.ParseUint(counter, 10, 64)
+			return p
+		}
+	}
+	return 0
+}
+
+// GenerateOTP return one time password or an error if occurs
 // The function compliant with RFC 4226, and implemented as mentioned in section 5.3
 // See https://tools.ietf.org/html/rfc4226#section-5.3
-func GeneratCode(otp OTP) (string, error) {
+func GeneratOTP(otp OTP) (string, error) {
 	secret := strings.ToUpper(otp.Secret())
 	key, err := base32.StdEncoding.DecodeString(secret)
 
@@ -37,7 +149,7 @@ func GeneratCode(otp OTP) (string, error) {
 	interval := make([]byte, 8)
 	binary.BigEndian.PutUint64(interval, otp.Interval())
 
-	hash := hmac.New(otp.Hasher(), key)
+	hash := hmac.New(otp.Algorithm().Hasher(), key)
 	_, err = hash.Write(interval)
 
 	if err != nil {
