c2b480555188d15866823689156d72510ef60edd
Sanad <s.hajyahya@f5.com>
2020-05-18 01:54:20 -0700

    docs: add tfa package docs

--- tfa/tfa.go
+++ tfa/tfa.go
@@ -1,3 +1,6 @@
+// Package tfa (two-factor authentication) provides a simple, clean, and idiomatic way
+// for generating and verifying one-time passwords
+// for both HOTP and TOTP defined in RFC 4226 and 6238.
 package tfa
 
 import (
