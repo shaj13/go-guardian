0fde23cd7095db9b926d4244f853bb56a74dfcc4
Sanad <s.hajyahya@f5.com>
2020-05-19 05:32:48 -0700

    test: add example for bearer and expose Token function

--- auth/strategies/bearer/bearer.go
+++ auth/strategies/bearer/bearer.go
@@ -11,7 +11,7 @@ import (
 
 var (
 	// ErrInvalidToken indicate a hit of an invalid bearer token format.
-	// And it's returned by authenticating functions or by static strategy load token form file.
+	// And it's returned by Token function.
 	ErrInvalidToken = errors.New("bearer: Invalid bearer token")
 	// ErrTokenNotFound is returned by authenticating functions for both cached and static bearer strategies when token not found in their store.
 	ErrTokenNotFound = errors.New("barer: Token does not exists")
@@ -22,23 +22,11 @@ var (
 type authenticateFunc func(ctx context.Context, r *http.Request, token string) (auth.Info, error)
 
 func (auth authenticateFunc) authenticate(ctx context.Context, r *http.Request) (auth.Info, error) {
-	header := r.Header.Get("Authorization")
-	header = strings.TrimSpace(header)
-
-	if header == "" {
-		return nil, ErrInvalidToken
+	token, err := Token(r)
+	if err != nil {
+		return nil, err
 	}
-
-	token := strings.Split(header, " ")
-	if len(token) < 2 || strings.ToLower(token[0]) != "bearer" {
-		return nil, ErrInvalidToken
-	}
-
-	if len(token[1]) == 0 {
-		return nil, ErrInvalidToken
-	}
-
-	return auth(ctx, r, token[1])
+	return auth(ctx, r, token)
 }
 
 // Append new token to a bearer strategy store.
@@ -73,4 +61,26 @@ func Revoke(strat auth.Strategy, token string, r *http.Request) error {
 	}
 
 	return ErrInvalidStrategy
+}
+
+// Token return bearer token from Authorization header, or ErrInvalidToken,
+// The returned token will not contain "Bearer" keyword
+func Token(r *http.Request) (string, error) {
+	header := r.Header.Get("Authorization")
+	header = strings.TrimSpace(header)
+
+	if header == "" {
+		return "", ErrInvalidToken
+	}
+
+	token := strings.Split(header, " ")
+	if len(token) < 2 || strings.ToLower(token[0]) != "bearer" {
+		return "", ErrInvalidToken
+	}
+
+	if len(token[1]) == 0 {
+		return "", ErrInvalidToken
+	}
+
+	return token[1], nil
 }
---
+++ auth/strategies/bearer/example_test.go
@@ -0,0 +1,124 @@
+package bearer
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/shaj13/go-passport/auth"
+	"github.com/shaj13/go-passport/storage"
+)
+
+func ExampleAppend() {
+	cache := storage.NewDefaultCache(time.Minute * 5)
+	strategy := NewCachedToken(NoOpAuthenticate, cache)
+	info := auth.NewDefaultUser("1", "2", nil, nil)
+	token := "90d64460d14870c08c81352a05dedd3465940a7"
+	r, _ := http.NewRequest("POST", "/login", nil)
+	// append new token to cached bearer strategy
+	err := Append(strategy, token, info, r)
+	fmt.Println(err)
+	// Output:
+	// <nil>
+}
+
+func ExampleRevoke() {
+	cache := storage.NewDefaultCache(time.Minute * 5)
+	strategy := NewCachedToken(NoOpAuthenticate, cache)
+	r, _ := http.NewRequest("GET", "/logout", nil)
+	// assume token extracted from header
+	token := "90d64460d14870c08c81352a05dedd3465940a7"
+	err := Revoke(strategy, token, r)
+	fmt.Println(err)
+	// Output:
+	// <nil>
+}
+
+func ExampleToken() {
+	r, _ := http.NewRequest("GET", "/logout", nil)
+	r.Header.Set("Authorization", "Bearer 90d64460d14870c08c81352a05dedd3465940a7")
+	token, err := Token(r)
+	fmt.Println(err, token)
+
+	r.Header.Set("Authorization", "90d64460d14870c08c81352a05dedd3465940a7")
+	token, err = Token(r)
+	fmt.Println(err, token)
+
+	// Output:
+	// <nil> 90d64460d14870c08c81352a05dedd3465940a7
+	// bearer: Invalid bearer token
+}
+
+func ExampleNewStaticFromFile() {
+	strategy, _ := NewStaticFromFile("testdata/valid.csv")
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.Header.Set("Authorization", "Bearer testUserToken")
+	info, err := strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info.ID())
+	// Output:
+	// <nil> 1
+}
+
+func ExampleNewStatic() {
+	strategy := NewStatic(map[string]auth.Info{
+		"90d64460d14870c08c81352a05dedd3465940a7": auth.NewDefaultUser("example", "1", nil, nil),
+	})
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.Header.Set("Authorization", "Bearer 90d64460d14870c08c81352a05dedd3465940a7")
+	info, err := strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info.ID())
+	// Output:
+	// <nil> 1
+}
+
+func ExampleNewCachedToken() {
+	authFunc := Authenticate(func(ctx context.Context, r *http.Request, token string) (auth.Info, error) {
+		fmt.Print("authFunc called ")
+		if token == "90d64460d14870c08c81352a05dedd3465940a7" {
+			return auth.NewDefaultUser("example", "1", nil, nil), nil
+		}
+		return nil, fmt.Errorf("Invalid user token")
+	})
+
+	cache := storage.NewDefaultCache(time.Minute * 5)
+	strategy := NewCachedToken(authFunc, cache)
+
+	r, _ := http.NewRequest("GET", "/", nil)
+	r.Header.Set("Authorization", "Bearer 90d64460d14870c08c81352a05dedd3465940a7")
+
+	// first request when authentication decision not cached
+	info, err := strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info.ID())
+
+	// second request where authentication decision cached and authFunc will not be called
+	info, err = strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info.ID())
+	// Output:
+	// authFunc called <nil> 1
+	// <nil> 1
+}
+
+func ExampleNoOpAuthenticate() {
+	cache := storage.NewDefaultCache(time.Microsecond * 500)
+	strategy := NewCachedToken(NoOpAuthenticate, cache)
+
+	// demonstrate a user attempt to login
+	r, _ := http.NewRequest("GET", "/login", nil)
+	// user verified and add the user token to startegy using append or cache
+	cache.Store("token", auth.NewDefaultUser("example", "1", nil, nil), r)
+
+	// first request where authentication decision added to cached
+	r, _ = http.NewRequest("GET", "/login", nil)
+	r.Header.Set("Authorization", "Bearer token")
+	info, err := strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info.ID())
+
+	// second request where authentication decision expired and user must login again
+	time.Sleep(time.Microsecond * 600)
+	info, err = strategy.Authenticate(r.Context(), r)
+	fmt.Println(err, info)
+	// Output:
+	// <nil> 1
+	// NOOP <nil>
+}
