9191354df90daeb9c5a079cb55b5d00e7685601b
shaj13 <hajsanad@gmail.com>
2020-05-23 09:32:50 -0700

    feat: add LRU cache

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/shaj13/go-guardian
 go 1.13
 
 require (
+	github.com/golang/groupcache v0.0.0-20200121045136-8c9f03a8e57e
 	github.com/gorilla/sessions v1.2.0
 	github.com/stretchr/testify v1.5.1
 )
---
+++ store/lru_test.go
@@ -0,0 +1,94 @@
+package store
+
+import (
+	"net/http"
+	"sync"
+	"testing"
+
+	"github.com/golang/groupcache/lru"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLRU(t *testing.T) {
+	table := []struct {
+		name        string
+		key         string
+		value       interface{}
+		op          string
+		expectedErr bool
+		found       bool
+	}{
+		{
+			name:  "it return false when key does not exist",
+			op:    "load",
+			key:   "key",
+			found: false,
+		},
+		{
+			name:  "it return true and value when exist",
+			op:    "load",
+			key:   "test",
+			value: "test",
+			found: true,
+		},
+		{
+			name:  "it overwrite exist key and value when store",
+			op:    "store",
+			key:   "test",
+			value: "test2",
+			found: true,
+		},
+		{
+			name:  "it create new record when store",
+			op:    "store",
+			key:   "key",
+			value: "value",
+			found: true,
+		},
+		{
+			name:  "it's not crash when trying to delete a non exist record",
+			key:   "key",
+			found: false,
+		},
+		{
+			name:  "it delete a exist record",
+			op:    "delete",
+			key:   "test",
+			found: false,
+		},
+	}
+
+	for _, tt := range table {
+		t.Run(tt.name, func(t *testing.T) {
+			cache := &LRU{
+				Cache: lru.New(2),
+				MU:    &sync.Mutex{},
+			}
+
+			cache.Cache.Add("test", "test")
+
+			r, _ := http.NewRequest("GET", "/", nil)
+			var err error
+
+			switch tt.op {
+			case "load":
+				v, ok, err := cache.Load(tt.key, r)
+				assert.Equal(t, tt.value, v)
+				assert.Equal(t, tt.found, ok)
+				assert.NoError(t, err)
+				return
+			case "store":
+				err = cache.Store(tt.key, tt.value, r)
+			case "delete":
+				err = cache.Delete(tt.key, r)
+			}
+
+			v, ok := cache.Cache.Get(tt.key)
+			assert.NoError(t, err)
+			assert.Equal(t, tt.found, ok)
+			assert.Equal(t, tt.value, v)
+
+		})
+	}
+
+}
