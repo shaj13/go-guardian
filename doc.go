319b0875ba4a9d2678b98f6d81fdd4dcc8c32dcf
Sanad Haj Yahya <65346528+shaj13@users.noreply.github.com>
2020-05-27 20:57:37 +0300

    Update doc.go

--- doc.go
+++ doc.go
@@ -1,4 +1,4 @@
-// Copyright 202 The Gorilla Go-Guardian. All rights reserved.
+// Copyright 2020 The Go-Guardian. All rights reserved.
 // Use of this source code is governed by a MIT
 // license that can be found in the LICENSE file.
 
