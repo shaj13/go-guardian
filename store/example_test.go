2a1eae9d8893b6236ea5ef595662002fbdebd06c
shaj13 <hajsanad@gmail.com>
2020-05-31 00:35:07 -0700

    remove session

--- store/example_test.go
+++ store/example_test.go
@@ -4,39 +4,12 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"sync"
 	"time"
 
 	"github.com/golang/groupcache/lru"
-	"github.com/gorilla/sessions"
 )
 
-func ExampleSession() {
-	r, _ := http.NewRequest("GET", "/", nil)
-	w := httptest.NewRecorder()
-	store := sessions.NewCookieStore([]byte("key"))
-	sessionCache := &Session{
-		Name:   "seesion-key",
-		GStore: store,
-	}
-
-	sessionCache.Store("key", "value", r)
-	SetCookie(w, r)
-
-	v, ok, _ := sessionCache.Load("key", r)
-	fmt.Println(v, ok)
-
-	sessionCache.Delete("key", r)
-	SetCookie(w, r)
-
-	v, ok, _ = sessionCache.Load("key", r)
-	fmt.Println(v, ok)
-
-	// Output:
-	// value true
-	// <nil> false
-}
 
 func ExampleNewFIFO() {
 	ctx, cancel := context.WithCancel(context.Background())
