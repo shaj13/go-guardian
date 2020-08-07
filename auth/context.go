c31102b3c4453dec54c2ebb965f2539f23b081c1
shaj13 <hajsanad@gmail.com>
2020-08-07 16:41:08 -0700

    fix: expose helper func to store/get user info in/from  ctx

--- auth/context.go
+++ auth/context.go
@@ -9,13 +9,23 @@ type userKey struct{}
 
 // RequestWithUser Save user information in request context.
 func RequestWithUser(info Info, r *http.Request) *http.Request {
-	ctx := context.WithValue(r.Context(), userKey{}, info)
+	ctx := CtxWithUser(r.Context(), info)
 	return r.WithContext(ctx)
 }
 
 // User return user information from request context.
 func User(r *http.Request) Info {
-	v := r.Context().Value(userKey{})
+	return UserFromCtx(r.Context())
+}
+
+// CtxWithUser Save user information in context.
+func CtxWithUser(ctx context.Context, info Info) context.Context {
+	return context.WithValue(ctx, userKey{}, info)
+}
+
+// UserFromCtx return user information from context.
+func UserFromCtx(ctx context.Context) Info {
+	v := ctx.Value(userKey{})
 	if info, ok := v.(Info); ok {
 		return info
 	}
