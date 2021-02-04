ff9dbb712b99403ea1af99eeb972c150dc0c5ad9
shaj13 <hajsanad@gmail.com>
2021-02-04 00:46:44 -0800

    fix: add username hashing

--- auth/strategies/basic/cached.go
+++ auth/strategies/basic/cached.go
@@ -5,24 +5,41 @@ import (
 	"net/http"
 
 	"github.com/shaj13/go-guardian/v2/auth"
+	"github.com/shaj13/go-guardian/v2/auth/internal"
 )
 
 // ExtensionKey represents a key for the password in info extensions.
 // Typically used when basic strategy cache the authentication decisions.
 const ExtensionKey = "x-go-guardian-basic-password"
 
+// NewCached return new auth.Strategy.
+// The returned strategy, caches the invocation result of authenticate function.
+func NewCached(f AuthenticateFunc, cache auth.Cache, opts ...auth.Option) auth.Strategy {
+	cb := new(cachedBasic)
+	cb.fn = f
+	cb.cache = cache
+	cb.comparator = plainText{}
+	cb.hasher = internal.PlainTextHasher{}
+	for _, opt := range opts {
+		opt.Apply(cb)
+	}
+	return New(cb.authenticate, opts...)
+}
+
 type cachedBasic struct {
 	fn         AuthenticateFunc
 	comparator Comparator
 	cache      auth.Cache
+	hasher     internal.Hasher
 }
 
 func (c *cachedBasic) authenticate(ctx context.Context, r *http.Request, userName, pass string) (auth.Info, error) { // nolint:lll
+	hash := c.hasher.Hash(userName)
 	v, ok := c.cache.Load(userName)
 
 	// if info not found invoke user authenticate function
 	if !ok {
-		return c.authenticatAndHash(ctx, r, userName, pass)
+		return c.authenticatAndHash(ctx, r, hash, userName, pass)
 	}
 
 	if _, ok := v.(auth.Info); !ok {
@@ -33,13 +50,13 @@ func (c *cachedBasic) authenticate(ctx context.Context, r *http.Request, userNam
 	ext := info.GetExtensions()
 
 	if !ext.Has(ExtensionKey) {
-		return c.authenticatAndHash(ctx, r, userName, pass)
+		return c.authenticatAndHash(ctx, r, hash, userName, pass)
 	}
 
 	return info, c.comparator.Compare(ext.Get(ExtensionKey), pass)
 }
 
-func (c *cachedBasic) authenticatAndHash(ctx context.Context, r *http.Request, userName, pass string) (auth.Info, error) { //nolint:lll
+func (c *cachedBasic) authenticatAndHash(ctx context.Context, r *http.Request, hash string, userName, pass string) (auth.Info, error) { //nolint:lll
 	info, err := c.fn(ctx, r, userName, pass)
 	if err != nil {
 		return nil, err
@@ -47,20 +64,7 @@ func (c *cachedBasic) authenticatAndHash(ctx context.Context, r *http.Request, u
 
 	hashedPass, _ := c.comparator.Hash(pass)
 	info.GetExtensions().Set(ExtensionKey, hashedPass)
-	c.cache.Store(userName, info)
+	c.cache.Store(hash, info)
 
 	return info, nil
-}
-
-// NewCached return new auth.Strategy.
-// The returned strategy, caches the invocation result of authenticate function.
-func NewCached(f AuthenticateFunc, cache auth.Cache, opts ...auth.Option) auth.Strategy {
-	cb := new(cachedBasic)
-	cb.fn = f
-	cb.cache = cache
-	cb.comparator = plainText{}
-	for _, opt := range opts {
-		opt.Apply(cb)
-	}
-	return New(cb.authenticate, opts...)
 }
